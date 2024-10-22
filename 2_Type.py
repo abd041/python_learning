1db1ba0fd068b9d08b9dafbdd3a146163a0addec
Muhammad Abdullah <mabdullah@ogoul.com>
2024-10-22 20:45:54 +0500

    typecatsing and input updated

---
+++ 2_Type.py
@@ -0,0 +1,7 @@
+a= "31.5"
+
+b = float(a)
+c=type(a)
+d=type(b)
+
+print(c , d)
---
+++ 3_input.py
@@ -0,0 +1,4 @@
+a = int(input("enter number 1 is "))
+b = int(input("enter number 2 is "))
+
+print("sum of two number is" , a + b)
