61f2cd125779dbd302561f05ee39fa3e145f4d90
Muhammad Abdullah <mabdullah@ogoul.com>
2024-11-04 16:44:40 +0500

    lists and lists methods completed

---
+++ lists and tuples/2_list_methods.py
@@ -0,0 +1,17 @@
+friends =["Haider" , "abdullah" , 1 , 7 , 10 , "Rauf"]
+
+numbersList=[15 , 45 , 10 , 0 ,1 ,18]
+
+# This will add or append a new element at the end of list 
+
+friends.append("Harry") 
+
+# This will sort the list
+
+numbersList.sort()
+
+# Insert at a numebr at specific index
+
+numbersList.insert(4 , 50)
+
+print(numbersList)
